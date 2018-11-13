fd72aaa1c85d21b646302eae1c939a07f093cbb6
slavailchenko35 <slavailchenko35@gmail.com>
2018-11-13 16:18:21 +0200

    v0.6

--- myEE1.js
+++ myEE1.js
@@ -2,12 +2,38 @@ class myEE {
 
 	constructor() {
 		this.listeners = new Map();
+		this.maxListeners = 3;
 	}
 
 	addListener (label, cb) {
-		this.listeners.has(label) || this.listeners.set(label, []);
-		this.listeners.get(label).push(cb);
-		console.log (this.listeners);
+		if (this.listenerCount(label) < this.maxListeners) {
+			this.listeners.has(label) || this.listeners.set(label, []);
+			this.listeners.get(label).push(cb);
+			console.log (`Added ${cb} in ${label}`);
+		} else {
+			console.log(`Number of handlers exceeded`);
+		}
+	}
+
+	on (label, cb) {
+		if (this.listenerCount(label) < this.maxListeners) {
+			this.listeners.has(label) || this.listeners.set(label, []);
+			this.listeners.get(label).push(cb);
+			console.log (`Added ${cb} in ${label}`);
+		} else {
+			console.log(`Number of handlers exceeded`);
+		}
+	}
+
+	once (label, cb) {
+		if (this.listenerCount(label) < this.maxListeners) {
+			this.listeners.has(label) || this.listeners.set(label, []);
+			this.listeners.get(label).push(cb);
+			console.log (`Added ${cb} in ${label}`);
+			cb.listenerEvent = false;
+		} else {
+			console.log(`Number of handlers exceeded`);
+		}
 	}
 
 	removeListener (label, cb) {
@@ -35,13 +61,63 @@ class myEE {
       return false;
   	}
 
+  	removeAllListeners ([...labels]) {
+
+		let listeners = labels.map(item => this.listeners.get(item));
+
+  		for (let i=0; i<listeners.length; i++) {
+  			if (this.listeners.has(labels[i])) {
+  				this.listeners.delete(labels[i]);
+  				console.log (`Removed listeners from ${labels[i]}`);
+  			} else {
+  				console.log (`${labels[i]} not founded`);
+  			}
+  		}
+	}
+
+	eventNames() {
+		let arr = [...this.listeners.keys()];
+		console.log(`Array of events ${arr}`);
+		return arr;
+     }
+
+    listenersArr (label) {
+        let arr = this.listeners.get(label);
+        return arr;
+    }
+
+    listenerCount(label) {
+    	let res = this.listenersArr(label);
+    	// console.log (`Count for event ${label}: ${res}`)
+    	if (res) return res.length
+    		else return 0;
+    }
+
+    setMaxListeners(n) {
+        this.maxListeners = n;
+    }
+
+    getMaxListeners() {
+        return this.maxListeners;
+    }
+
   	emit(label, ...args) {
   		let listeners = this.listeners.get(label);
 
   		if (listeners && listeners.length) {
   			listeners.forEach((listener) => {
-  				listener(...args); 
-  			});
+
+  			if (listener.hasOwnProperty('listenerEvent') && 
+  				listener.listenerEvent === true) {
+  				this.removeListener (label, listener)	
+  			};
+
+  			if (listener.hasOwnProperty('listenerEvent') && 
+  				listener.listenerEvent === false) {
+  				listener.listenerEvent = true;
+  			}
+  			listener(...args);
+  		});
   			return true;
   		}
   		return false;
@@ -50,12 +126,27 @@ class myEE {
 }
 
 let events = new myEE ();
-let cb1 = () => console.log ('Added event1 in Listener');
+let cb1 = () => console.log ('Added cb1 in Listener');
 events.addListener ('Event 1', cb1);
-let cb2 = () => console.log ('Added event2 in Listener');
+let cb2 = () => console.log ('Added cb2 in Listener');
+let cb3 = () => console.log ('Added cb3 in Listener');
+let cb4 = () => console.log ('Added cb4 in Listener');
 events.addListener ('Event 1', cb2);
 events.addListener ('Event 2', cb2);
+events.addListener ('Event 2', cb3);
+events.addListener ('Event 2', cb4);
+events.addListener ('Event 2', cb1);
+events.once ('Event 3', cb3);
+events.addListener ('Event 3', cb1);
+events.removeAllListeners(['Event 4', 'Event 5']);
 events.removeListener ('Event 1', cb2);
 events.removeListener ('Event 3', (() => console.log('Hello World')));
+events.eventNames();
 events.emit ('Event 1');
 events.emit ('Event 2');
+events.emit ('Event 3');
+events.listenersArr ('Event 3');
+events.emit ('Event 2');
+events.emit ('Event 3');
+events.listenersArr ('Event 3');
+events.listenerCount ('Event 3');
