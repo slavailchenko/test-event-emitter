8faadbec5d65e05927f3d7165974e3b3f96c228d
slavailchenko35 <slavailchenko35@gmail.com>
2018-11-17 01:16:38 +0200

    v0.98

--- task3.js
+++ task3.js
@@ -1,69 +1,78 @@
 const express = require('express');
 const fs = require('fs');
-const {Transform} = require('stream');
+const stream = require('stream');
 const JSONStream = require('JSONStream');
-const es = require('event-stream');
-const csv = require('csv');
-
-// const oppressor = require('oppressor');
-// const bodyparser = require('body-parser');
+const jsonToCsv = require("json-to-csv-stream");
+const Converter=require("csvtojson").Converter;
+const csvConverter=new Converter({output: "csv", });
+// const csvToJSON = require('csvtojson');
 const crypto = require('crypto');
 
 function scheduleGc () {
-	
-	if (!global.gc) {
-		console.log('Garbage collection is not exposed');
-		return;
+	const limitMemory = 157286400;
+	console.log('Memory usage', process.memoryUsage());
+	if (process.memoryUsage().heapUsed > limitMemory) {
+		console.log('Exit....');
+		process.exit(-1);
 	}
-
-	const limitMemory = 15728640;
-
-	setTimeout(() => {
-		global.gc();
-		console.log('Memory usage', process.memoryUsage());
-		if (process.memoryUsage().heapUsed < limitMemory) {
-			scheduleGc();
-		} else {
-			console.log('Exit....');
-			process.exit(-1);
-		}
-	}, 500);
 };
 
-fs.stat('cars.csv', ((err, stat) => {
-	if (err == null) {
-		console.log('File cars.csv exists');
-		fs.unlink('cars.csv', (error) => {
-			if (!error) {
-				console.log('File cars.csv removed');
-			} else {
-				console.log ('Error deleting the file');
-			}
-		})
-	} else {
-		console.log('File cars.csv not exists');
-	}
-})
-);
+const isTimeUsedMemory = setInterval(scheduleGc, 500);
 
-es.pipeline(
-	fs.createReadStream('cars1.json'),
-	JSONStream.parse(),
-	es.map((data, next) => {
+const fileRemove = (file) => {
+	fs.unlink(file, (error) => {
+		if (!error) {
+			console.log(`File ${file} removed`);
+		} else {
+			console.log ('Error deleting the file');
+		}
+	});
+};
 
-		data.hash = 33333;
-		console.log(data);
-		console.log(next);
+fileRemove('cars.csv');
 
-		next(null, [data])
-	}),
-	csv().to('cars1.csv')
-	);
-
-// const stream = fs.createReadStream('cars.json', {bufferSize: 64 * 1024})
-// let data = stream.pipe(process.stdout);
-// console.log(stream);
-
-scheduleGc ();
-
-
+fs.createReadStream('cars.json', { highWaterMark: 4096 }).
+pipe(JSONStream.parse('*')).
+pipe(
+	new stream.Transform ({
+		objectMode: true,
+		highWaterMark: 4,
+		transform: (chunk, encode, callback) => {
+			const { make, model, displayName, dealershipId } = chunk;
+			chunk.hash = crypto.createHash('md5').update(make + model + displayName + dealershipId).digest('hex');
+			callback(null, JSON.stringify(chunk));
+			// console.log(chunk);
+		}
+	})
+	).pipe(jsonToCsv()).
+pipe(
+	fs.createWriteStream('cars.csv', { highWaterMark: 4096 })
+	).
+on('finish', () => fileRemove('cars2.json')).
+on('finish', () => {
+	fs.createReadStream('cars.csv', { highWaterMark: 2048 }).
+	pipe(csvConverter).
+	pipe(
+        new stream.Transform ({
+        	objectMode: true,
+        	highWaterMark: 4,
+        	transform: (chunk, encode, callback) => {
+        		let buff = JSON.parse(chunk.toString());
+        		this.count = ++this.count || 1;
+        		buff.id = this.count;
+        		buff.vehicleId = Array(8).join().replace(/(.|$)/g, () => {
+        			return ((Math.random()*36)|0).toString(36)
+        		});
+        		callback(null, JSON.stringify(buff))
+        	}
+        })
+      ).pipe(
+      fs.createWriteStream('cars2.json', {
+      	highWaterMark: 2048
+      })
+      ).
+      on('finish', () => {
+      	clearInterval (isTimeUsedMemory);
+        console.log ('All completed');
+      });
+  });
