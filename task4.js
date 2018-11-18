89b41a0d28fa42e02e7fa19a21d979ca15b5b96a
slavailchenko35 <slavailchenko35@gmail.com>
2018-11-18 23:03:51 +0200

    v1.15

--- task4.js
+++ task4.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const server = require('http').createServer(app);
+const bodyParser = require('body-parser');
 
 // app.all('*', (req, res, next) => {
 //     setTimeout(() => next(), 80000);
 // });
+
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
 app.get('/cars', ((req, res, next) => {
     if (req.path == '/cars') {
@@ -17,10 +20,10 @@ app.get('/cars', ((req, res, next) => {
 })
 );
 
-server.listen(8081, err => {
+app.listen(8081, err => {
     if (err) {
         console.log('Server creation error: ' + err);
         return;
     }
     console.log(`Server started on port`);
-}).setTimeout(1600000);
+}).setTimeout(1800000);
