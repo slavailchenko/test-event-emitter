4077415be8fefbe30f3453e503fec9d02e843004
slavailchenko35 <slavailchenko35@gmail.com>
2018-11-20 23:08:40 +0200

    Done task3.1

--- task3/task3.js
+++ task3/task3.js
@@ -10,12 +10,12 @@ const crypto = require('crypto');
 // Timer on use memory
 
 const scheduleGc = () => {
-  const limitMemory = 15000000;
-  console.log('Memory usage', process.memoryUsage());
-  if (process.memoryUsage().heapUsed > limitMemory) {
-    console.log('Exit....');
-    process.exit(-1);
-  }
+    const limitMemory = 15000000;
+    console.log('Memory usage', process.memoryUsage());
+    if (process.memoryUsage().heapUsed > limitMemory) {
+        console.log('Exit....');
+        process.exit(-1);
+    }
 };
 
 const isTimeUsedMemory = setInterval(scheduleGc, 500);
@@ -23,13 +23,13 @@ const isTimeUsedMemory = setInterval(scheduleGc, 500);
 // check and remove file
 
 const fileRemove = (file) => {
-	fs.unlink(file, (error) => {
-		if (!error) {
-			console.log(`File ${file} removed`);
-		} else {
-			console.log ('Error deleting the file');
-		}
-	});
+    fs.unlink(file, (error) => {
+        if (!error) {
+            console.log(`File ${file} removed`);
+        } else {
+            console.log ('Error deleting the file');
+        }
+    });
 };
 
 let i;
@@ -37,25 +37,25 @@ let i;
 // functions of transform
 
 const readTransformJSON = (chunk, encode, callback) => {
-  i = ++i || 1;
-  const { make, model, displayName, dealershipId } = chunk;
-  chunk.hash = crypto.createHash('md5').update(make + model + displayName + dealershipId).digest('hex');
-  // console.log (i);
-  callback(null, JSON.stringify(chunk));
-}
+    i = ++i || 1;
+    const { make, model, displayName, dealershipId } = chunk;
+    chunk.hash = crypto.createHash('md5').update(make + model + displayName + dealershipId).digest('hex');
+    // console.log (i);
+    callback(null, JSON.stringify(chunk));
+};
 
 const readTransformCSV = (chunk, encode, callback) => {
-  let buff = JSON.parse(chunk.toString());
-  this.count = ++this.count || 1;
-  buff.id = this.count;
-  buff.vehicleId = Array(8).join().replace(/(.|$)/g, () => {
-    return ((Math.random()*36)|0).toString(36)
-  });
-  let str = (buff.id == 1) ? '['+'\r\n'+JSON.stringify(buff)+','+'\r\n' : 
-            (buff.id == i) ? JSON.stringify(buff)+'\r\n'+']' :
+    let buff = JSON.parse(chunk.toString());
+    this.count = ++this.count || 1;
+    buff.id = this.count;
+    buff.vehicleId = Array(8).join().replace(/(.|$)/g, () => {
+        return ((Math.random()*36)|0).toString(36)
+    });
+    let str = (buff.id === 1) ? '['+'\r\n'+JSON.stringify(buff)+','+'\r\n' :
+        (buff.id === i) ? JSON.stringify(buff)+'\r\n'+']' :
             JSON.stringify(buff)+','+'\r\n';
-  callback(null, str);
-}
+    callback(null, str);
+};
 
 fileRemove('cars.csv');
 
@@ -64,28 +64,28 @@ fileRemove('cars.csv');
 fs.createReadStream('cars.json', {highWaterMark: 4096}).
 pipe(JSONStream.parse('*')).
 pipe(
-  new stream.Transform ({
-    objectMode: true,
-    highWaterMark: 4,
-    transform: readTransformJSON
-  })
-  ).
+    new stream.Transform ({
+      objectMode: true,
+      highWaterMark: 4,
+        transform: readTransformJSON
+    })
+).
 pipe(jsonToCsv()).
 pipe(fs.createWriteStream('cars.csv', {highWaterMark: 4096})).
 on('finish', () => fileRemove('cars2.json')).
 on('finish', () => {
-  fs.createReadStream('cars.csv', {highWaterMark: 2048}).
-  pipe(csvConverter).
-  pipe(
-    new stream.Transform ({
-      objectMode: true,
-      highWaterMark: 4,
-      transform: readTransformCSV
-    })
+    fs.createReadStream('cars.csv', {highWaterMark: 2048}).
+    pipe(csvConverter).
+    pipe(
+        new stream.Transform ({
+            objectMode: true,
+            highWaterMark: 4,
+            transform: readTransformCSV
+        })
     ).
-  pipe(fs.createWriteStream('cars2.json', {highWaterMark: 2048})).
-  on('finish', () => {
-    clearInterval (isTimeUsedMemory);
-    console.log ('All completed');
-  })
+    pipe(fs.createWriteStream ('cars2.json', {highWaterMark: 2048})).
+    on('finish', () => {
+        clearInterval (isTimeUsedMemory);
+        console.log ('All completed');
+    })
 });
